d6e47298ce6e8e1fe6f94471764e1a21de7c99df
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2023-11-05 13:14:22 +0100

    Update 0_Animation_Demo.py

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,17 +1,3 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from typing import Any
 
 import numpy as np
@@ -67,7 +53,7 @@ def animation_demo() -> None:
     # Streamlit widgets automatically run the script from top to bottom. Since
     # this button is not connected to any other logic, it just causes a plain
     # rerun.
-    st.button("Re-run")
+    st.button("Re-peatrun")
 
 
 st.set_page_config(page_title="Animation Demo", page_icon="📹")
