c6492f66c9c07cb42a1eaba003f8ffdeea96f76e
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2023-11-05 14:42:00 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-
 st.markdown(
         """
         Salut à toi, futur ami de SOHAN! 
         Voici quelques premières premières photos de bébé SOHAN, avant que tu ne le rencontres
         si ce n'est déjà fait!
+
+        Choisis une page à gauche!
         """)
