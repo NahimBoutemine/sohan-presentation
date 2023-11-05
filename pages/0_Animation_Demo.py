f8c8bf022fcb651719b5f9684169697b07966b50
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2023-11-05 14:56:05 +0100

    Update 0_Animation_Demo.py

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -4,17 +4,9 @@ import numpy as np
 
 import streamlit as st
 
-def animation_demo() -> None:
-
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
+st.markdown("# MATERNITE")
 st.sidebar.header("Animation Demo")
 st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
+    """voici les premiers jours de bébé, à la maternité
 )
 
-animation_demo()
-
-show_code(animation_demo)
